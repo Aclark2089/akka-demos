9ba0c719edf4967371301ba5cb7541ffd707f37e
R. Alex Clark <aclark2089@gmail.com>
2018-06-19 01:14:22 -0400

    Added comment docs for remote actor app

--- src/main/java/com/develop/sample/akka/remote/RemoteActorApp.java
+++ src/main/java/com/develop/sample/akka/remote/RemoteActorApp.java
@@ -19,19 +19,24 @@ public class RemoteActorApp {
         schedulePrinterMessages();
     }
 
+    // Load the remote creation system with specifically the 'remotecreation.conf' file settings
     private static void startCreationSystem() {
         creationSystem = ActorSystem.create("RemoteCreationSystem", ConfigFactory.load("remotecreation"));
     }
 
+    // Load the remote creation system with specifically the 'remoteprinter.conf' file settings
     private static void startPrinterSystem() {
         printerSystem = ActorSystem.create("RemotePrinterSystem", ConfigFactory.load("remoteprinter"));
     }
+
 
     private static void schedulePrinterMessages() {
 
         final ActorRef creator = creationSystem.actorOf(RemoteCreationActor.createProps(), "creationActor");
         final Random randomizer = new Random();
 
+        // Send random messages that will be run on multiple ports
+        // Could also be set up to run on more than one system with tcp connection
         creationSystem.scheduler()
                 .schedule(
                         Duration.create(1, TimeUnit.SECONDS),   // Delay before start
